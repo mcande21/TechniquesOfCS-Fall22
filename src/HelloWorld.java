fab582feabb0e18ef9ec351f0e161a2bd2df24b7
mcande21 <candersoan@gocva.com>
2022-08-30 14:56:27 -0400

    My first java program.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
+
     }
+
 
 }
