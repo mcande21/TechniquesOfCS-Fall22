f23ece980132ec393921409e8c5ae4a42cd1fe60
mcande21 <candersoan@gocva.com>
2022-08-30 15:14:00 -0400

    My first java program.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,20 @@
 public class HelloWorld {
 
+        /*
+        This is a multi-line lock comment.
+        Another comment line here.
+        See what I mean, jelly bean.
+         */
     public static void main(String[] args) {
         System.out.println("Hello World");
 
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniqures Of COmputer Science";
+        double avagadros = 6.022140857e23; // Scientific Notation
+        double electron_mass = 9.1093837015e-31; // negative scientific notation
+
 
     }
 
