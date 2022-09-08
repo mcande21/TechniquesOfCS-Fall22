0b482aff51ef0a7ee1ff1b9cbaf7fda833b8d268
mcande21 <candersoan@gocva.com>
2022-09-08 13:41:23 -0400

    DayOfWeek.java

--- src/HW2/DayOfWeek.java
+++ src/HW2/DayOfWeek.java
@@ -1,4 +1,27 @@
 package HW2;
 
+import java.util.Scanner;
+
 public class DayOfWeek {
+
+    public static double dayoftheweek(int month, int day, int year){
+        int year_1 = year - ( 14 - month) / 12;
+        int x = year_1 + year_1 / 4 - year_1 / 100 + year_1 / 400;
+        int month_1 = month + 12 * ((14 - month) / 12) - 2;
+        int day_of_week = (day + x + 31 * month_1 / 12) % 7;
+        return day_of_week;
+    }
+
+    public static void main(String [] args){
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Enter a month(mm): ");
+        int month_value = kbd.nextInt();
+        System.out.print("Enter in the day(dd): ");
+        int day_value = kbd.nextInt();
+        System.out.print("Enter in the year(yyyy): ");
+        int year_value = kbd.nextInt();
+        System.out.printf("This date falls on the %.0f day of the week.",
+                dayoftheweek(month_value, day_value, year_value));
+
+    }
 }
---
+++ src/investmentCalculator.java
@@ -0,0 +1,30 @@
+import utility.Investment;
+
+import java.util.Scanner;
+
+
+public class investmentCalculator {
+
+    public static void main(String [] args) {
+
+        // prompt the user for c, r, t, n
+        // and call the function with the inputted values
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("Enter in your initial investment: ");
+        double c_value = kbd.nextDouble();
+
+        System.out.print("Enter in your rate: ");
+        double r_value = kbd.nextDouble();
+
+        System.out.print("Enter in your years: ");
+        int t_value = kbd.nextInt();
+
+        System.out.print("Enter in your compound: ");
+        int n_value = kbd.nextInt();
+
+        System.out.printf("Your amount comes out to: %.2f. ",
+                Investment.investment(c_value, r_value, t_value, n_value));
+
+    }
+}
