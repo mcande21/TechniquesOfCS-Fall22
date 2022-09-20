b5536e796410d0a12f825c5d6ea1411c78cb9092
mcande21 <candersoan@gocva.com>
2022-09-20 12:44:14 -0400

    investment.java

--- src/HW3/Investment.java
+++ src/HW3/Investment.java
@@ -5,42 +5,90 @@ import java.util.Scanner;
 
 public class Investment {
 
-    public static double investment(double c, double r, int t, int n){
-        return c * Math.pow(1 + r/n, t*n);
+    public static double investment(double c, double r, int t, int n) {
+        return c * Math.pow(1 + r / n, t * n);
     }
-    public static void exit(){}
-
-    public static void main(String [] args) {
-        Scanner kbd = new Scanner(System.in);
-        System.out.print("Please enter your initial investment: \n");
+    public static double get_cost(Scanner s) {
         double cost = 0;
-        if (kbd.hasNextDouble()){
-            cost = kbd.nextDouble();}
-        else {
-            System.out.println("The value you entered does not compute. Please try again.");
-            exit();}
-        System.out.print("Please enter your rate of investment: \n");
+        while (true) {
+            Scanner kbd = new Scanner(System.in);
+            System.out.print("Please enter your initial investment: ");
+            if (kbd.hasNextDouble()) {
+                cost = kbd.nextDouble();
+                if (cost < 0) {
+                    System.out.println("The value you entered is not a positive number. Please try again");
+                }
+                else{
+                    break;
+                }}
+            else {
+                System.out.println("Error: The value you entered does not compute. Please try again");
+            }}
+        return cost;
+    }
+    public static double get_rate(Scanner s) {
         double rate = 0;
-        if (kbd.hasNextDouble()){
-            rate = kbd.nextDouble();}
-        else {
+        while (true){
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Please enter your rate of investment(in decimal form): ");
+        if (kbd.hasNextDouble()) {
+            rate = kbd.nextDouble();
+            if (rate < 0 || rate > 1){
+                System.out.println("The rate you entered is below zero. That makes no sense. Please try again");
+            }
+            else {
+                break;
+            }
+        } else {
             System.out.println("The value you entered does not compute. Please try again.");
-            exit();}
-        System.out.print("Please enter your time frame: \n");
-        int year = 0;
-        if (kbd.hasNextInt()){
-            year = kbd.nextInt();}
-        else {
-            System.out.println("The value you entered does not compute. Please try again.");
-            exit();}
-        System.out.print("PLease enter the amount you are compounding per year: \n");
-        int n = 0;
-        if (kbd.hasNextInt()){
-            n = kbd.nextInt();}
-        else {
-            System.out.println("The value you entered does not compute. Please try again.");
-            exit();}
+        }}
+        return rate;
+    }
 
+    public static int get_time(Scanner s) {
+        int year = 0;
+        while(true){
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("Please enter your time frame: ");
+        if (kbd.hasNextInt()) {
+            year = kbd.nextInt();
+            if (year < 0){
+                System.out.println("The year is a negative. That makes no sense.");
+            }
+            else {
+                break;
+            }
+        } else {
+            System.out.println("The value you entered does not compute. Please try again.");
+        }}
+        return year;
+    }
+
+    public static int get_compound(Scanner s) {
+        int n = 0;
+        while (true){
+        Scanner kbd = new Scanner(System.in);
+        System.out.print("PLease enter the amount you are compounding per year: ");
+        if (kbd.hasNextInt()) {
+            n = kbd.nextInt();
+            if (n < 0){
+                System.out.println("The rate you compound is a negative. That makes no sense. Please try again");
+            }
+            else{
+                break;
+            }
+        } else {
+            System.out.println("The value you entered does not compute. Please try again.");
+        }}
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        double cost = get_cost(kbd);
+        double rate = get_rate(kbd);
+        int year = get_time(kbd);
+        int n = get_compound(kbd);
         System.out.printf("The total value of your investment is $%.2f",
                 investment(cost, rate, year, n));
 
--- src/utility/Weather.java
+++ src/utility/Weather.java
@@ -4,45 +4,60 @@ import java.util.Scanner;
 
 public class Weather {
 
-    public static double windchill(double degrees, double wind_speed){
+    public static double windchill(double degrees, double wind_speed) {
         double t = degrees;
         double V = Math.pow(wind_speed, 0.16);
         double W = 35.75 + 0.6215 * t + (0.4275 * t - 35.75) * (V);
         return W;
     }
-    public static void main(String [] args) {
+
+    public static double getVelocity(Scanner s) {
+        boolean valid = false;
+        double velocity = 0;
+        while (true) {
+            System.out.print("Enter wind velocity in degrees MPH: ");
+            if (s.hasNextDouble()) {
+                velocity = s.nextDouble();
+                if (velocity < 0) {
+                    System.out.printf("Error: velocity cannot be negative. You entered %.2f\n", velocity);
+                } else {
+                    break;
+                }
+            } else {
+                System.out.printf("Error: you did not enter a valid wind speed. You entered \"%s\" \n",
+                        s.next());
+            }
+        }
+        return velocity;
+    }
+
+    public static double getTemp(Scanner s) {
+        boolean valid = true;
+        double temperature = 0;
+        while (true) {
+            System.out.print("Enter temperature in degrees F: ");
+            if (s.hasNextDouble()) {
+                temperature = s.nextDouble();
+            } else {
+                System.out.printf("Error: please enter a valid number. You entered \"%s\"\n",
+                        s.next());
+            }
+            return temperature;
+        }
+    }
+
+    public static void main(String[] args) {
 
         // used for getting input from the keyboard
         Scanner kbd = new Scanner(System.in);
 
-        System.out.println("Enter temperature in degrees F: ");
-        double temperature;
-
-        if (kbd.hasNextDouble() == true) {
-            temperature = kbd.nextDouble();
-        }
-        else {
-            System.out.printf("Error: please enter a valid number. You entered \"%s\"",
-                  kbd.next());
-            return; // exit main
-        }
-
-        System.out.print("Enter wind velocity in degrees MPH: ");
-        double velocity;
-        if (kbd.hasNextDouble()) {
-            velocity = kbd.nextDouble();
-        }
-        else{
-            System.out.printf("Error: you did not enter a valid wind speed. You entered \"%s\"", kbd.next());
-            return;
-        }
+        double velocity = getVelocity(kbd);
+        double temperature = getTemp(kbd);
 
         System.out.printf("Windchill is %.2f\n", windchill(temperature, velocity));
         System.out.printf("The Windchill for %.0f degrees and %.0f MPH winds is: %.2f degrees\n",
-                temperature, velocity, windchill(temperature,velocity));
+                temperature, velocity, windchill(temperature, velocity));
         // System.out.println("They said \"Don't do it!!\".");
-
-
     }
 
 }
